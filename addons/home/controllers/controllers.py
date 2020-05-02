12da625edc3aaa073ac55ce70d74a3ad0f5f8db0
Dajalvo <dajalvo2020@gmail.com>
2020-05-02 13:23:53 -0500

    Se agrega el módulo home

---
+++ addons/home/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+ class Home(http.Controller):
+     @http.route('/home/home/', auth='public')
+     def index(self, **kw):
+         return "Hello, world"
+
+     @http.route('/home/home/objects/', auth='public')
+     def list(self, **kw):
+         return http.request.render('home.listing', {
+             'root': '/home/home',
+             'objects': http.request.env['home.home'].search([]),
+         })
+
+     @http.route('/home/home/objects/<model("home.home"):obj>/', auth='public')
+     def object(self, obj, **kw):
+         return http.request.render('home.object', {
+             'object': obj
+         })
