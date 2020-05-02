910c9c7c732b0ae22433b967cec0ffa5e9f028e0
Dajalvo <dajalvo2020@gmail.com>
2020-05-02 14:12:10 -0500

    Correción controlador módulo home 4

--- addons/home/__manifest__.py
+++ addons/home/__manifest__.py
@@ -1,35 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "home",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': 'Home',
+    'category': 'Tools',
+    'summary': 'Centralize your address book',
     'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+This module gives you a quick view of your contacts directory, accessible from your home page.
+You can track your vendors, customers and other contacts.
+""",
+    'depends': ['base', 'mail'],
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/contact_views.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'application': True,
 }
