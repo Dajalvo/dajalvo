4be39a8dffde0a237a850274d9471cd032fb3c2d
root <root@localhost.localdomain>
2020-09-09 22:24:53 -0500

    Se agrega posible módulo para conuntos residenciales

--- addons/account/__manifest__.py
+++ addons/account/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Contabilidad',
+    'name' : 'Accounting',
     'version' : '1.1',
     'summary': 'Accounting, Invoices & Payments',
     'sequence': 15,
